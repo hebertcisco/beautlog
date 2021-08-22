1f88884dabd244cdad0b9aba9123d9812ef23877
hebertcisco <hebertcisco@outlook.com>
2021-08-22 15:16:38 +0000

    test: :white_check_mark: add unity tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,52 @@
-import message from '../index';
+import Console, { print, printf, log, debug, puts } from '../index';
 
-it('Works', () => {
-  expect(message).toBe('This module must not be required!');
+const PHRASE = 'Hello World';
+
+it(`puts returns ${PHRASE}`, () => {
+  return puts(`${PHRASE}`)
+});
+
+it(`print returns ${PHRASE}`, () => {
+  return print(`${PHRASE}`)
+});
+
+it(`printf returns ${PHRASE}`, () => {
+  return printf(`${PHRASE}`)
+});
+
+it(`log returns ${PHRASE}`, () => {
+  return log(`${PHRASE}`)
+});
+
+it(`debug returns ${PHRASE}`, () => {
+  return debug(`${PHRASE}`)
+});
+
+//  Console __tests__
+it(`Console.ok returns ${PHRASE}`, () => {
+  return Console.ok(`${PHRASE}`)
+});
+
+it(`Console.success returns ${PHRASE}`, () => {
+  return Console.success(`${PHRASE}`)
+});
+
+it(`Console.trace returns ${PHRASE}`, () => {
+  return Console.trace(`${PHRASE}`)
+});
+
+it(`Console.warn returns ${PHRASE}`, () => {
+  return Console.warn(`${PHRASE}`)
+});
+
+it(`Console.warning returns ${PHRASE}`, () => {
+  return Console.warning(`${PHRASE}`)
+});
+
+it(`Console.error returns ${PHRASE}`, () => {
+  return Console.error(`${PHRASE}`)
+});
+
+it(`Console.info returns ${PHRASE}`, () => {
+  return Console.info(`${PHRASE}`)
 });
