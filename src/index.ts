7c8e69f41fd0015510137661e2e4111b36f4e684
hebertcisco <hebertcisco@outlook.com>
2021-08-22 14:40:33 +0000

    feat: :art: adding exports ways

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export default 'This module must not be required!';
+import Console from './core/log';
+
+export const print = Console.info
+export const printf = Console.info
+export const log = Console.info
+export const puts = Console.info
+export const debug = Console.debug
+
+export * as logger from './core/log';
+export * as bl from './core/log';
+export default Console;
